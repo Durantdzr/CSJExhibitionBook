cb27caacdcac4ab2d8535a57ddd9df68fd66b03d
durant <duzr@shdata.com>
2024-07-12 21:31:03 +0800

    新增接口调用手机号 调整用户id通过header获取X-WX-OPENID

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -2,81 +2,10 @@ from datetime import datetime
 from flask import render_template, request
 from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid, \
-    insert_book_record, get_book_available,delete_bookbyid
+    insert_book_record, get_book_available, delete_bookbyid
 from wxcloudrun.model import Counters, Book_Record
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-
-
-@app.route('/')
-def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
-
-
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
-    params = request.get_json()
-
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
-
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
-
-
-@app.route('/api/user', methods=['GET'])
-def insert_user():
-    """
-    :return: 计数的值
-    """
-    record = Book_Record()
-    record.userid = '123'
-    record.create_time = datetime.now()
-    insert_book_record(record)
-    return make_succ_response(0) if record is None else make_succ_response(record.id)
+import requests
 
 
 @app.route('/api/get_available_num', methods=['GET'])
@@ -99,7 +28,7 @@ def book_record():
     # if 'action' not in params:
     #     return make_err_response('缺少action参数')
 
-    record.userid = params.get("userid")
+    record.userid = request.headers['X-WX-OPENID']
     record.book_type = params.get("book_type")
     record.book_num = params.get("book_num")
     record.booker_name = params.get("booker_name")
@@ -114,7 +43,7 @@ def get_book_history():
     """
         :return:历史预约记录列表
     """
-    userid = request.args.get('userid')
+    userid = request.headers['X-WX-OPENID']
     records = Book_Record.query.filter(Book_Record.userid == userid).all()
     result = []
     for record in records:
@@ -132,7 +61,7 @@ def get_book_record():
     """
     if datetime.now() > datetime(datetime.now().year, datetime.now().month, 16):
         return make_succ_response([])
-    userid = request.args.get('userid')
+    userid = request.headers['X-WX-OPENID']
     records = Book_Record.query.filter(Book_Record.userid == userid).filter(
         Book_Record.book_mouth == datetime.now().strftime('%Y-%m')).filter(Book_Record.status == 1).all()
     result = []
@@ -147,7 +76,7 @@ def get_book_record():
 @app.route('/api/delete_record', methods=['POST'])
 def delete_record():
     """
-    :return:提交预约
+    :return:删除预约
     """
 
     # 获取请求体参数
@@ -155,3 +84,18 @@ def delete_record():
 
     delete_bookbyid(params.get("id"))
     return make_succ_response(0)
+
+
+@app.route('/api/get_user_phone', methods=['GET'])
+def get_user_phone():
+    """
+    :return:获取手机号
+    """
+
+    # 获取请求体参数
+    wxOpenid = request.headers['X-WX-OPENID']
+    params = request.get_json()
+    result = requests.post('http://api.weixin.qq.com/wxa/getopendata', params={"openid": wxOpenid},
+                           data={'cloudid_list': [params.get("cloudid")]})
+
+    return make_succ_response(result[0]['json']['data']['phoneNumber'])
