b30088e773b8e27f88e5cef7f380b1192648a57b
durant <duzr@shdata.com>
2024-08-01 13:50:58 +0800

    查询剩余数量接口增加字段

--- wxcloudrun/dao.py
+++ wxcloudrun/dao.py
@@ -45,13 +45,15 @@ def get_book_available_openday(openday=datetime.now().strftime('%Y-%m-%d')):
     if openday_available is None:
         return None
     available_list = {"上午": openday_available.people_AM, '下午': openday_available.people_PM}
+    interval={"上午": str(openday_available.begintime_AM) + ":00-" + str(openday_available.endtime_AM) + ":00",
+                                  "下午": str(openday_available.begintime_PM) + ":00-" + str(openday_available.endtime_PM) + ":00"}
     use_num = Book_Record.query.with_entities(Book_Record.book_type,
                                               db.func.sum(Book_Record.book_num).label('use_num')).filter(
         Book_Record.status == 1, Book_Record.openday == openday).group_by(
         Book_Record.book_type).all()
     for item in use_num:
         available_list[item.book_type] = int(available_list[item.book_type] - item.use_num)
-    return [{"type": key, "avaliable_num": value} for key, value in available_list.items()]
+    return [{"type": key, "avaliable_num": value,"interval":interval.get(key)} for key, value in available_list.items()]
 
 
 def get_book_available_bytype(book_type, openday):
